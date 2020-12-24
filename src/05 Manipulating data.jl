f39ace498d5a00fcd8071780ca52b51413a0f26d
juanklopper <juan.klopper@uct.ac.za>
2020-12-24 10:00:01 +0200

    Added content to 05

--- src/05 Manipulating data.jl
+++ src/05 Manipulating data.jl
@@ -4,9 +4,9 @@
 # INTRODUCTION
 
 # The DataFramesMeta package adds six macros
-# to the three macros in the DataFrames package (@select
-# @transform, and @combine)
-# Provides a convenient way to select and manipulate
+# to the select, transformm, and combine functions in
+# the DataFrames package
+# It provides a convenient way to select and manipulate
 # data
 
 
@@ -26,6 +26,54 @@ df = DataFrame(ID = 1:10, Var1 = rand(1:10, 10), Var2 = rand(-50:50, 10) ./ 10, 
 # Two variables with 10 rows of data
 
 
+# SPLIT-APPLY-COMBINE
+
+# This is a strategy to group data, apply functions
+# to each group, and combine the data again
+# The groupyby function creates a GroupedDataFrame object
+# The combine, select or select! and the transform or
+# transform! functions can be used on a GroupedDataFrame
+# These functions also act of AbstractDataFrame objects (when
+# the groupby function is not used)
+
+
+# CREATING A GROUPEDDATAFRAME object
+
+gdf = groupby(df, :Cat1) # Group by sample space
+# elements in the Cat1 variable
+
+gdf[(Cat1 = "I", )] # View only selected subdataframe
+
+
+# CALCULATE A STATISTIC
+
+combine(gdf, nrow) # Number of cases per class
+
+combine(gdf, :Var1 => sum) # Sum of Var1
+
+combine(gdf, :Var1 => mean) # Mean of Var1 per
+# Cat1 element
+
+combine(gdf, :Var1 => mean => :mean) # Name the
+# new column
+
+combine(gdf, nrow, :Var1 => mean => :average)
+# More than one calculation
+
+combine(gdf, AsTable([:Var1, :Var2]) =>
+    x -> mean(x.Var1) / mean(x.Var2)) # More than
+# one variable
+
+combine(x -> mean(x.Var1) / std(x.Var1), gdf)
+# Function notation
+
+combine(gdf, :Var1 => (x -> [extrema(x)]) => [:min, :max])
+
+combine(gdf) do df
+    (μ = mean(df.Var1), σ = std(df.Var1), e = extrema(df.Var1))
+end # Using a do end block
+
+
 # SELECT SPECIFIC DATA
 
 @select(df, :Var1) # Select only the first variable and
@@ -33,13 +81,6 @@ df = DataFrame(ID = 1:10, Var1 = rand(1:10, 10), Var2 = rand(-50:50, 10) ./ 10, 
 
 @select(df, :Var2, Var3 = abs.(:Var2)) # Select a column
 # and create a new one by transforming another
-
-
-# GROUP BY VALUES FOR A VARIABLE
-
-grouped_df = groupby(df, :Cat1) # Create a grouped dataframe object
-
-@select(grouped_df, :Var1) # Grouping variable is included
 
 
 # TRANSFORM DATA
@@ -53,6 +94,6 @@ grouped_df = groupby(df, :Cat1) # Create a grouped dataframe object
 
 @where(df, :Var2 .< 0, :Cat1 .== "I") # Adding more constraints
 
-@where(df, :Var2 .> mean(:Var2)) # Selection criteria based one
+@where(df, :Var2 .> mean(:Var2)) # Selection criteria based on
 # a statistic
 
